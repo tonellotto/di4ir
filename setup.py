fa0f60054d31d212b96b9f2d140f31cf1b9d44f2
Nicola Tonellotto <tonellotto@users.noreply.github.com>
2021-09-28 15:27:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 readme = ''
 
 setup(
-    name='DeepImpact',
+    name='deepimpact',
     version='0.0.1',
     url='https://github.com/tonellotto/di4ir',
     packages=['deepimpact'],
